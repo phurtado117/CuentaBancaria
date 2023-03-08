3e80b2ed72cbcc1b3219c1b0991c808b56896384
Pablo Hurtado <phurtado127@gmail.com>
2023-03-08 16:19:02 -0600

    se crea la clase CuentaBancaria

--- src/main/java/CuentaBBVA.java
+++ src/main/java/CuentaBBVA.java
@@ -1,2 +1,82 @@
-package PACKAGE_NAME;public class CuentaBBVA {
-}
+import java.util.Scanner;
+public class CuentaBBVA {
+    private String name;
+    private int Account;
+    private int balance;
+
+
+
+    public CuentaBBVA(String name, int Account, int balance) {
+        this.Account = Account;
+        this.name = name;
+        this.balance = balance;
+
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAccount() {
+        return Account;
+    }
+
+    public void setAccount(int account) {
+        Account = account;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+
+    }
+    public void Menu(){
+        Scanner entra =new Scanner(System.in);
+        int opcion;
+        double Retirar =0;
+        double total;
+        double Deposito =0;
+
+        do{
+            System.out.println("[1] Saldo");
+            System.out.println("[2] Retirar");
+            System.out.println("[3] Depositar");
+            System.out.println("[0] Salir");
+            opcion= entra.nextInt();
+
+            if(opcion == 1){
+                System.out.println("su saldo es;"+getBalance());
+            }
+            if (opcion==2){
+                System.out.println("ingrese cantidad a retirar:");
+                Retirar =entra.nextDouble();
+                total=getBalance()-Retirar;
+                System.out.println("su saldo ahora es:"+total);
+            }
+            if (opcion==3){
+                System.out.println("ingrese cantidad a depositar");
+                Deposito= entra.nextDouble();
+                total=getBalance()+Deposito;
+                System.out.println("su saldo ahora es:"+total);
+
+
+            }
+
+        }
+        while(opcion!=0);
+
+            }
+
+        }
+
+
+
+
+
+
--- src/main/java/TestCuentaBBVA.java
+++ src/main/java/TestCuentaBBVA.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class TestCuentaBBVA {
+
+import java.io.IOException;
+import java.util.Scanner;
+public class TestCuentaBBVA {
+    public static void main(String[] args) throws IOException{
+
+        CuentaBBVA Account = new CuentaBBVA("pablo",8989898,890);
+
+        Account.Menu();
+
+
+
+    }
+
+
 }
